7fed82755fc0181320720d6d5ca889633e0df334
Mouse Mousecheese_tech Cheese_Technology <mousecheese_tech@Mouses-MacBook-Air.local>
2025-02-13 11:05:54 +0530

    vercelchange

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/database');
 const adminRoutes = require('./routes/adminRoutes');
 const hunterRoutes = require('./routes/hunterRoutes');
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
@@ -25,6 +34,8 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT ,() => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app
