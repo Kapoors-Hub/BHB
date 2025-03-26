a4b1eee6ed6dcfd7def07c79c1112258763dd24b
Mouse Mousecheese_tech Cheese_Technology <mousecheese_tech@Mouses-MacBook-Air.local>
2025-03-26 12:10:54 +0530

    senses

--- controllers/hunterQuizController.js
+++ controllers/hunterQuizController.js
@@ -2,6 +2,7 @@
 const Quiz = require('../models/Quiz');
 const QuizAttempt = require('../models/QuizAttempt');
 const Hunter = require('../models/Hunter');
+const notificationController = require('./notificationController');
 
 const hunterQuizController = {
     // Start a new quiz attempt
