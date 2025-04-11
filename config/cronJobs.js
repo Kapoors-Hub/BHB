cc5d6ad2a8dc65f6ceaeefb30e56690940b2b10a
Mouse Mousecheese_tech Cheese_Technology <mousecheese_tech@Mouses-MacBook-Air.local>
2025-04-11 17:08:42 +0530

    senses

--- config/cronJobs.js
+++ config/cronJobs.js
@@ -9,7 +9,7 @@ const TitleAward = require('../models/TitleAward');
 
 const initCronJobs = () => {
     // Add this to your config/cronJobs.js file
-    cron.schedule('00 08 * * *', async () => {
+    cron.schedule('50 16 * * *', async () => {
         try {
 
             // Use IST (UTC+5:30)
